547c83cc4791f3beaa62c3c64c489107efa0a03d
Monique Oda <57405516+odamonique@users.noreply.github.com>
2025-09-28 22:01:03 +0200

    Create function to play a round

--- my-script.js
+++ my-script.js
@@ -40,3 +40,64 @@ function getHumanChoice() {
 const humanChoice = getHumanChoice();
 //Check if returned value works as expected 
 console.log(humanChoice);
+
+//Create function to play a round 
+function playRound(humanChoice, computerChoice) {
+    
+    //Make human choice case-insensitive
+    humanChoice = humanChoice.toLowerCase();
+
+    //Compare choices to check the winner 
+    //If human choice is the same as computer 
+    if (humanChoice === computerChoice) {
+        //Show a tie message 
+        console.log("It's a tie!");
+    }
+    //If human choice is Rock and computer choice is scissors 
+    else if (humanChoice === "rock" & computerChoice === "scissors"){
+        //Show a winner message
+        console.log("You win! Rock beats scissors");
+        //Increment human score
+        humanScore++;
+    }
+    //If human choice is scissors and computer choice is rock
+    else if (humanChoice === "scissors" & computerChoice === "rock"){
+        //Show loser message
+        console.log("You lose! Rock beats scissors");
+        //Increment computer score
+        computerScore++;
+    }
+    //If human choice is scissors and computer choice is paper
+    else if (humanChoice === "scissors" & computerChoice === "paper"){
+        //Show winner message 
+        console.log("You win! Scissors beats paper");
+        //Increment human score
+        humanScore++;
+    }
+    //If human choice is paper and computer choice is scissors
+    else if (humanChoice === "paper" & computerChoice === "scissors") {
+        //Show loser massage
+        console.log("You lose! Scissors beats paper");
+        //Increment computer score
+        computerScore++;
+    }
+    //If human choice is paper and computers choice is rock
+    else if (humanChoice === "paper" & computerChoice === "rock") {
+        //Show winner message 
+        console.log("You win! Paper beats rock");
+        //Increment human score
+        humanScore++;
+    }
+    //If human choice is rock and computer choice is paper 
+    else
+    {
+        //Show loser message 
+        console.log("You lose! Paper beats rock");
+        //Increment computer score
+        computerScore++;
+    }
+    
+}
+
+//Call function playRound and pass the user and computers choice as arguments 
+playRound(humanChoice, computersChoice);
